443c8c17c28db05e5cd2632026af97bfc3fd7114
Sjoerd Crijns <s.crijns@umcutrecht.nl>
2012-06-30 11:53:50 +0200

    add method to set root node rectangle

--- src/quadtree.h
+++ src/quadtree.h
@@ -1,11 +1,17 @@
-template <typename T> class QuadNode;
+#include <QRectF>
 
+template <typename T> class QuadNode;
 template <typename T>
+
 class QuadTree
 {
 public:
   void insert(T*);
+  void setRootRectangle(QRectF r);
   
 protected:
   QuadNode<T*> *rootnode;
 };
+
+template <typename T>
+inline void QuadTree<T>::setRootRectangle(QRectF r) { rootnode->setNodeRectangle(r); }
