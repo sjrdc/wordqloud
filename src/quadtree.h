41b487798ef0563c37bb7afce225011e78674df8
Sjoerd Crijns <s.crijns@umcutrecht.nl>
2012-06-30 13:03:23 +0200

    add query method to tree class

--- src/quadtree.h
+++ src/quadtree.h
@@ -1,17 +1,28 @@
 #include <QRectF>
 
+template <typename T> class QList;
 template <typename T> class QuadNode;
-template <typename T>
 
+template <typename T>
 class QuadTree
 {
 public:
   void insert(T*);
   void setRootRectangle(QRectF r);
-  
+  void query(QRectF r, QList<T*> &l) const;
 protected:
   QuadNode<T> *rootnode;
 };
 
 template <typename T>
-inline void QuadTree<T>::setRootRectangle(QRectF r) { rootnode->setNodeRectangle(r); }
+inline void QuadTree<T>::setRootRectangle(QRectF r) 
+{ 
+  rootnode->setNodeRectangle(r); 
+}
+
+template <typename T>
+inline void QuadTree<T>::query(QRectF r, QList<T*> &l) const 
+{
+  rootnode->query(r, l); 
+}
+
