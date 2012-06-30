6307e287b2b28e736c3912494c69fef4e3dbc85e
Sjoerd Crijns <s.crijns@umcutrecht.nl>
2012-06-30 11:49:50 +0200

    implement initBranches method

--- src/quadnode.cpp
+++ src/quadnode.cpp
@@ -3,25 +3,58 @@
 template <typename T> 
 void QuadNode<T>::insert(T* t)
 {
-  if (leafs.isEmpty())
-    initLeafs();
+  if (contents.size() >= maxContents && branches.isEmpty())
+    initBranches();
 
-  foreach (QuadNode<T> *node, leafs)
+  // try to insert in child nodes
+  foreach (QuadNode<T> *node, branches)
     if (node->contains(t))
       {
 	node->insert(t);
 	return;
       }
 
-  // else...
+  // else... add item to our own collection
   contents.push_back(t);
 }
 
 template <typename T>
-void QuadNode<T>::initLeafs()
+void QuadNode<T>::initBranches()
 {
-  if (nodeRectangle.width() * nodeRectangle.height() < minimumNodeArea)
-    return;
+  QRectF r = this->nodeRectangle;
+  r.setSize(0.5*r.size());
+  
+  QuadNode<T> *lnw = new QuadNode();
+  r.moveTo(this->nodeRectangle.topLeft());
+  lnw.setNodeRectangle(r);
+  branches.push_back(lnw);
 
-  /// \todo further init leafs
+  QuadNode<T> *lne = new QuadNode();
+  r.moveTo(this->nodeRectangle.topLeft() + QPointF(r.width(), 0.));
+  lne.setNodeRectangle(r);
+  branches.push_back(lne);
+
+  QuadNode<T> *lsw = new QuadNode();
+  r.moveTo(this->nodeRectangle.topLeft() + QPointF(0., r.height()));
+  lsw.setNodeRectangle(r);
+  branches.push_back(lsw);
+
+  QuadNode<T> *lse = new QuadNode();
+  r.moveTo(this->nodeRectangle.topLeft() + QPointF(r.width(), r.height()));
+  lse.setNodeRectangle(r);
+  branches.push_back(lse);
+
+  QList<T*> content2Remove;
+  foreach (T* item, contents)
+    {
+      foreach (QuadNode* node, branches)
+	{
+	  if (node->contains(item))
+	    {
+	      node->insert(item);
+	      content2Remove.push_back(item);
+	    }
+	}
+    }
+  contents.remove(content2Remove);
 }
--- src/quadnode.h
+++ src/quadnode.h
@@ -2,20 +2,22 @@
 #include <QRectF>
 
 static const float minimumNodeArea = 10.;
+static const float maxContents = 10.;
 
 template <typename T>
 class QuadNode
 {
 public:
   void insert(T*);
-  
+  void setNodeRectangle(QRectF r);
 protected:
   bool contains(T*);
-  void initLeafs();
+  void initBranches();
 
   QList<T*> contents;
-  QList<QuadNode<T*>*> leafs;
+  QList<QuadNode<T>*> branches;
   QRectF nodeRectangle;
 };
 
-
+template <typename T>
+inline void QuadNode<T>::setNodeRectangle(QRectF r) { nodeRectangle = r; }
