b512a37fb2a35efe77306d789f9f058f49cf4cc7
Sjoerd Crijns <s.crijns@umcutrecht.nl>
2012-07-01 22:44:57 +0200

    implement method to read list of words with modifiers

--- src/wordlist.cpp
+++ src/wordlist.cpp
@@ -42,3 +42,60 @@ void WordList::fromTextFile(QString filename)
 	}
     }
 }
+
+void WordList::fromWordFile(QString filename)
+{
+  QFile file(filename);
+  if (!file.open(QIODevice::ReadOnly)) 
+    qDebug() << "Could not read from file " << filename << ".";
+  
+  QTextStream stream(&file); 
+  QStringList stringlist;
+  while (!stream.atEnd() && stream.status() == QTextStream::Ok)
+    {
+      QString line = stream.readLine();
+      QStringList words = line.split(' ');
+
+      foreach (QString string, words)
+	if (!string.isEmpty())
+	  {
+	    // create a word
+	    Word *word = new Word(string);
+	    
+	    // find modifiers
+	    QStringList modifiers = string.split(':');
+	    foreach (QString modifier, modifiers)
+	      {
+		char key = modifier[0].toAscii();
+		switch(key)
+		  {
+		  case '#':
+		    {
+		      if (modifier.size() == 7)
+			word->setBrush(QColor(modifier));
+		      break;
+		    }
+		  case '@':
+		    {
+		      double angle = modifier.right(1).toDouble();
+		      break;
+		    }
+		  case '!':
+		    {
+		      word->setFontName(modifier.right(1));
+		      break;
+		    }
+		  default: 
+		    {
+		      int fsize = modifier.toInt();
+		      word->setFontSize(fsize);
+		      break;
+		    }
+		  }
+	      }
+
+	    this->push_back(word);
+	  }
+    }
+  file.close();
+}
