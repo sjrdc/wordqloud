2551b7dd045e14a2fb6295c2a23c6f4f91d31268
Sjoerd Crijns <s.crijns@umcutrecht.nl>
2012-07-09 16:29:26 +0200

    use opencv to obtain polygons that mark drawing region

--- src/boundsdialog.cpp
+++ src/boundsdialog.cpp
@@ -65,20 +65,45 @@ void BoundsDialog::onFileChanged(QString filename)
 
 void BoundsDialog::onSliderValueChanged(int v)
 {
-  CvMat *tmp = cvCreateMat(img->rows, img->cols, img->type);
-  cvThreshold(img, tmp, v, 255, CV_THRESH_BINARY);
-  
+  CvMat *tmp = cvCreateMat(img->rows, img->cols, CV_8UC1);
+  cvCvtColor(img, tmp, CV_BGR2GRAY);
+  cvThreshold(tmp, tmp, v, 255, CV_THRESH_BINARY);
+
+  CvSeq* contour = 0;
+  CvMemStorage* storage = cvCreateMemStorage(0);
+  cvFindContours(tmp, storage, &contour,
+		 sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+  QVector<QPolygon> polygons;
+  for (; contour != 0; contour = contour->h_next)
+    {
+      QPolygon polygon;
+      for (int i = 0; i < contour->total; ++i)
+	{
+	  CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, contour, i);
+	  polygon.push_back(QPoint(p->x, p->y));	  
+	}
+      polygons.push_back(polygon);
+    }
+
   QImage tmpimg = *orgImage;
   for (int i = 0; i < tmpimg.width(); ++i)
     for (int j = 0; j < tmpimg.height(); ++j)
       {
   	QRgb c = tmpimg.pixel(i, j);
   	tmpimg.setPixel(i, j, c < QColor(v, v, v).rgb() ? 
-  		     QColor(0, 0, 0).rgb() : QColor(255, 255, 255).rgb());
+			QColor(0, 0, 0).rgb() : QColor(255, 255, 255).rgb());
       }
 
+  QPainter p(&tmpimg);
+  p.setPen(Qt::red);
+  foreach (QPolygon polygon, polygons)
+    p.drawPolygon(polygon);
+
+  // previewLabel->setPixmap(pixmap->scaled(previewLabel->width(), 
+  // 					 previewLabel->height(), 
+  // 					 Qt::KeepAspectRatio));
+
   pixmap = new QPixmap(QPixmap::fromImage(tmpimg));
-  previewLabel->setPixmap(pixmap->scaled(previewLabel->width(), 
-  					 previewLabel->height(), 
-  					 Qt::KeepAspectRatio));
+  previewLabel->setPixmap(*pixmap);
 }
